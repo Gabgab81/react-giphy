1c961b3ed4d8abcf82f64d5577a270dbd3c88d97
gabriel roux <rouxgab81@gmail.com>
2023-02-23 17:54:20 -0500

    save

--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 
 class Gif extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.id !== this.props.id;
+  }
+
   handleClick = (event) => {
     this.props.selectGifFunct(this.props.id)
   };
 
   render() {
-    if (!this.props.id){
-      return null
+    if (!this.props.id) {
+      return null;
     }
+    console.log("gif render");
     const src = `https://media1.giphy.com/media/${ this.props.id }/giphy.gif`
     return(
       <img src={src} alt='' className='gif' onClick={this.handleClick} />
--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -4,24 +4,29 @@ class SearchBar extends Component {
   constructor(props) {
     super(props)
 
-    this.state = {
-      term: ""
-    };
+    // this.state = {
+    //   term: ""
+    // };
+  }
+
+  shouldComponentUpdate(nextPropsa, nextState) {
+    return false;
   }
 
   handleUpdate = (event) => {
     // debugger
-    this.setState({
-      term: event.target.value
-    });
-    console.log(this.state.term);
+    // this.setState({
+    //   term: event.target.value
+    // });
+    // console.log(this.state.term);
     this.props.searchFunction(event.target.value);
   };
 
   render() {
+    console.log("search bar render");
     return (
       <input
-        value={this.state.term}
+        // value={this.state.term}
         type="text"
         className="form-control form-search"
         onChange={this.handleUpdate}
